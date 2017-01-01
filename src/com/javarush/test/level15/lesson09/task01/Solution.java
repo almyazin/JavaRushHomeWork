a146cc4560e3809534333d29d455fdbb3b8727d0
Azm <alexandr.myazin@gmail.com>
2017-01-01 15:01:27 +0200

    complete task

--- src/com/javarush/test/level15/lesson09/task01/Solution.java
+++ src/com/javarush/test/level15/lesson09/task01/Solution.java
@@ -10,6 +10,15 @@ import java.util.Map;
 public class Solution {
     public static Map<Double, String> labels = new HashMap<Double, String>();
 
+    static
+    {
+        labels.put(1.0, "1.0");
+        labels.put(2.0, "2.0");
+        labels.put(3.0, "3.0");
+        labels.put(4.0, "4.0");
+        labels.put(5.0, "5.0");
+    }
+
     public static void main(String[] args) {
         System.out.println(labels);
     }

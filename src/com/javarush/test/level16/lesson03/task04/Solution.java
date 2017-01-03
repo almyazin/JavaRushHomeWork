91a6ecceea3c22d019f08f62532447a8c7cec416
Azm <alexandr.myazin@gmail.com>
2017-01-03 21:45:34 +0200

    complete task

--- src/com/javarush/test/level16/lesson03/task04/Solution.java
+++ src/com/javarush/test/level16/lesson03/task04/Solution.java
@@ -7,6 +7,17 @@ package com.javarush.test.level16.lesson03.task04;
 */
 
 public class Solution {
+    public static class SpecialThread implements Runnable
+    {
+        @Override
+        public void run()
+        {
+            for (StackTraceElement element : Thread.currentThread().getStackTrace())
+            {
+                System.out.println(element);
+            }
+        }
+    }
     public static void main(String[] args) throws InterruptedException {
         Thread thread = new Thread(new SpecialThread());
         thread.start();

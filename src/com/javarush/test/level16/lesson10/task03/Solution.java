06d657fd0d3c7c75bf9dddf333d0e8fea6178e66
Azm <alexandr.myazin@gmail.com>
2017-01-05 01:28:23 +0200

    complete task

--- src/com/javarush/test/level16/lesson10/task03/Solution.java
+++ src/com/javarush/test/level16/lesson10/task03/Solution.java
@@ -7,10 +7,21 @@ package com.javarush.test.level16.lesson10.task03;
 
 public class Solution {
     public static void main(String[] args) throws InterruptedException {
-        //Add your code here - добавь код тут
+        TestThread trd = new TestThread();
+        trd.start();
+        trd.interrupt();
     }
 
     //Add your code below - добавь код ниже
-    public static class TestThread {
+    public static class TestThread extends Thread
+    {
+        @Override
+        public void run()
+        {
+            while (!isInterrupted())
+            {
+
+            }
+        }
     }
 }
